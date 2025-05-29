4a23078a83d51d20c20f2463ef23f756bc93d693
olezha <sarban7@mail.ru>
2025-05-29 21:03:57 +0300

    add entity and settings

---
+++ src/main/java/com/example/relaxer/entity/Account.java
@@ -0,0 +1,7 @@
+package com.example.relaxer.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Account {
+}
---
+++ src/main/resources/db/migration/V1__create_entity.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA IF NOT EXISTS users_schema;
+
+CREATE TABLE IF NOT EXISTS t_users(
+    id INTEGER PRIMARY KEY NOT NULL,
+    c_name VARCHAR(50) NOT NULL,
+    c_age INTEGER CHECK(c_age > 0 AND c_age < 100),
+    c_passport_id INTEGER UNIQUE,
+    FOREIGN KEY (c_passport_id) REFERENCES t_passports(id)
+);
+
+CREATE TABLE IF NOT EXISTS t_passports(
+    id INTEGER PRIMARY KEY NOT NULL,
+    c_number VARCHAR(12) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS t_accounts(
+    id INTEGER PRIMARY KEY NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    c_user_id INTEGER,
+    FOREIGN KEY (c_user_id) REFERENCES t_users(id)
+);
+
+CREATE TABLE IF NOT EXISTS t_hobbies(
+    id INTEGER PRIMARY KEY NOT NULL,
+    c_type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS t_users_hobbies(
+    c_user_id INTEGER,
+    c_hobby_id INTEGER,
+    PRIMARY KEY(c_user_id, c_hobby_id),
+    FOREIGN KEY(c_user_id) REFERENCES t_users(id),
+    FOREIGN KEY(c_hobby_id) REFERENCES t_hobbies(id)
+);
