e40948b077b4bae82ab95d69657ed703334d1fd0
sarban <sarban7@mail.ru>
2025-06-05 19:28:07 +0300

    add ExceptionHandler

---
+++ src/main/java/com/example/relaxer/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.relaxer.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundErrors(NoSuchElementException ex) {
+        Map<String, String> errors = Map.of("message", ex.getMessage());
+        return ResponseEntity.status(404).body(errors);
+    }
+}
