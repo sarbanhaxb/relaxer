c1920422ef4ddb7a0224b206f87b4cae872cc80c
olezha <sarban7@mail.ru>
2025-06-06 20:19:08 +0300

    Add swagger config

---
+++ src/main/java/com/example/relaxer/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package com.example.relaxer.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Manager API")
+                        .version("1.0.0")
+                        .description("Документация API для управления пользователями")
+                        .contact(new Contact()
+                                .name("Ваше имя")
+                                .email("you@example.com")
+                                .url("https://example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSEE-2.0.html")));
+    }
+}
